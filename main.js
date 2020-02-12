94ef63e8d5b1f5c90fa89152d8cd5c3f94b2d834
tfyd <qwq37529810@gmail.com>
2020-02-11 18:29:24 -0800

    test

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function prepareMobile() {
         containerWidth = 500;
         cellSpace = 20;
         cellSideLength = 100;
-        console.log("not mobile");
+        console.log("not mobile!");
     }
 
     $('#grid-container').css('width', containerWidth - 2*cellSpace);
